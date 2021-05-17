70acb714c9429669a42bdd0356361f5cf6d84a08
Ethernok <emilio98gl@gmail.com>
2021-05-17 15:58:40 +0200

    Todo terminado, zoom, FAQ y banner

--- js/app.js
+++ js/app.js
@@ -1,56 +1,71 @@
-$(document).ready(function(){
-   
-    //-----------------Zoom del carrusel-----------------------
-    $("#zoom-in").on('click', (e) =>{
-        $('#myCarousel').css('transform', 'scale(1.5)')
-        $('.item-active img').css('max-height', '850px')
-        $('.item-active img').css('width', '90%')
-        console.log($('.item-active img').css('width'))
-        
-    })
-    $("#zoom-out").on('click', (e) =>{
-        $('#myCarousel').css('transform', 'scale(1.0)')
-        $('.item-active img').css('width', '65%')
-        $('.item-active img').css('max-height', '500px')
-        console.log(e.target)
-        
-    })
+$(document).ready(function () {
 
+
+
+  //-------------Animaciones de banner------------------
+  $(".banner").css('background-color', 'white')
+
+
+  // Intervalo de colores
+
+  // Se hace el banner que se pueda mover a través de la página
+  $(function () {
+    $(".relative").draggable();
+  });
+
+  $(".close-banner").on('click', () => {
+
+    $(".banner").effect('clip', {}, 1000, () => {
+      $('.relative').hide();
+    });
+    $(".close-banner").effect('clip', {}, 1000);
+
+
+  })
+
+  //----------------------Fin animaciones banner-------------------------
+
+  //-------------------Faq---------------------------
+  $("#faq-questions").accordion({
+    collapsible: true,
+    heightStyle: "content"
+  });
+  var faqActive = true;
+  $('#faq-button').on('click', () => {
+    faqActive = !faqActive;
+    if (faqActive == false) {
+      $(".ui-accordion-content").show();
+    } else {
+      $(".ui-accordion-content").hide();
+    }
+    $(".ui-accordion .ui-accordion-content").css('padding', 0)
+
+  })
+  //------------------------FIN Faq--------------------------
+
+  //----------------------Modal-------------------------------
+
+  $('.item').on('click', (ev) => {
+    if (ev.target.src != undefined) {
+      $('.overlay-img').remove();
+      $('.modal-dialog').prepend(`
+      <img class="overlay-img" src="${ev.target.src}">
+      `)
+      $('#modal-img').toggleClass('fade');
+    }
+
+    console.log(ev.target.src)
+    console.log(ev)
+  })
+  //-----------------Zoom del modal-----------------------
+  $("#zoom-in").on('click', () => {
+    $('.overlay-img').css({width:"+=80px" })
+    console.log($('.overlay-img').css('width'))
+
+  })
+  $("#zoom-out").on('click', () => {
+    $('.overlay-img').css({width:"-=80px" })
     
 
-   //-------------Animaciones de banner------------------
-    $(".banner").css('background-color', 'white')
-
-    $( function() {
-        $( ".relative" ).draggable();
-      } );
-    
-    $(".close-banner").on('click', ()=>{
-    
-    $( ".banner" ).effect('clip',{}, 1000, ()=>{$('.relative').hide();});
-    $( ".close-banner" ).effect('clip',{}, 1000);
-
-   
-    }) 
-
-    //----------------------Fin animaciones banner-------------------------
-
-    //-------------------Faq---------------------------
-    $( "#faq-questions" ).accordion({
-        collapsible: true,
-        heightStyle: "content"
-      });
-      var faqActive = true;
-      $('#faq-button').on('click', ()=>{
-        faqActive = !faqActive;
-        if(faqActive == false){
-          $(".ui-accordion-content").show();
-        }else{
-          $(".ui-accordion-content").hide();
-        }
-        $(".ui-accordion .ui-accordion-content").css('padding', 0)
-        
-      })
+  })
 })
-
-  
